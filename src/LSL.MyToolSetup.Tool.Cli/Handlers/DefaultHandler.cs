d94d714b31d58cc420fffd2ba19a92206307a43d
Alun Jones <alunacjones@gmail.com>
2024-09-23 15:41:07 +0100

    more bits

--- src/LSL.MyToolSetup.Tool.Cli/Handlers/DefaultHandler.cs
+++ src/LSL.MyToolSetup.Tool.Cli/Handlers/DefaultHandler.cs
@@ -16,10 +16,39 @@ public class DefaultHandler : IAsyncHandler<Default>
 
     public async Task<int> ExecuteAsync(Default _)
     {
-        var copy = CopyResourceToOutputBuilder(Directory.GetCurrentDirectory());
+        var basePath = Directory.GetCurrentDirectory();
+
+        string ToAbolsutePath(string path) => Path.Join(basePath, path);
+        string ToRelativePath(string path) => path.Replace(basePath, string.Empty).TrimStart('\\');
+
+        var copy = CopyResourceToOutputBuilder(basePath);
         await copy("appveyor.yml", "appveyor.yml");
         await copy("LSL.snk.enc", "LSL.snk.enc");
+        var projectFolder = ToRelativePath(Directory.GetDirectories(ToAbolsutePath("src"), "*.Cli").First());
+        var projectFile = Directory.GetFiles(ToAbolsutePath(projectFolder), "*.csproj").First();
 
+        var name = new DirectoryInfo(basePath).Name;
+        var content = await File.ReadAllTextAsync(projectFile);
+        content = content.Replace("<Project Sdk=\"Microsoft.NET.Sdk\">",
+            """
+            <Project Sdk="Microsoft.NET.Sdk">
+                <PropertyGroup>
+                    <SnkFile>../../LSL.snk</SnkFile>
+                </PropertyGroup>
+
+                <PropertyGroup Condition="Exists('$(SnkFile)')">
+                    <AssemblyOriginatorKeyFile>$(SnkFile)</AssemblyOriginatorKeyFile>
+                    <SignAssembly>True</SignAssembly>
+                </PropertyGroup>
+            
+            """.ReplaceLineEndings()
+        )
+        .Replace("<Authors>authors-here</Authors>", "<Authors>alunacjones</Authors>")
+        .Replace("<RepositoryUrl>https://github.com/your/repo-url</RepositoryUrl>", $"<RepositoryUrl>https://github.com/alunacjones/{name}</RepositoryUrl>")
+        .Replace("<PackageProjectUrl>https://github.com/your/project-url</PackageProjectUrl>", $"<PackageProjectUrl>https://github.com/alunacjones/{name}</PackageProjectUrl>");
+
+        await File.WriteAllTextAsync(projectFile, content.Replace("${WorkingFolder}", new DirectoryInfo(basePath).Name));
+        
         return 0;
     }
 
